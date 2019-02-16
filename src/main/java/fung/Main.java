a04951770dbd8a85d6c7e4a194055155d8339ec1
LaoBing_Fung <FLB_B401@163.com>
2019-02-17 00:06:13 +0800

    buy specified amount of chicken with specified money

--- src/main/java/fung/Main.java
+++ src/main/java/fung/Main.java
@@ -1,4 +1,9 @@
 package main.java.fung;
 
+import main.java.fung.service.BuyChicken;
+
 public class Main {
+    public static void main(String[] args) {
+        BuyChicken.buyChicken(100.0, 100);
+    }
 }
--- src/main/java/fung/service/BuyChicken.java
+++ src/main/java/fung/service/BuyChicken.java
@@ -1,4 +1,22 @@
 package main.java.fung.service;
 
+import main.java.fung.model.Chick;
+import main.java.fung.model.Hen;
+import main.java.fung.model.Rooster;
+
 public class BuyChicken {
+    private static Rooster rooster = new Rooster();
+    private static Hen hen = new Hen();
+    private static Chick chick = new Chick();
+
+    public static void buyChicken(double cost, int total) {
+        for (int i = 0; i <= cost / rooster.getPrice(); i++) {
+            for (int j = 0; j <= cost / hen.getPrice(); j++) {
+                int k = total - i - j;
+                if ((i * rooster.getPrice() + j * hen.getPrice() + k * chick.getPrice()) == cost) {
+                    System.out.println("方案：" + i + "只公鸡" + j + "只母鸡" + k + "只小鸡");
+                }
+            }
+        }
+    }
 }
